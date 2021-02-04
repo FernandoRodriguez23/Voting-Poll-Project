791d2977c51e9b494dcc3259a43a495ec286d819
Fernando Rodriguez Agredano <Frodri300@west-mec.local>
2021-02-03 18:21:50 -0700

    Nixon got drip

--- script.js
+++ script.js
@@ -2,3 +2,18 @@
 function disCliamer() {
     alert("Please be sure to cast your vote for the next president");
 }
+
+function changeGeorge(){
+    document.getElementById("bGeorge").innerHTML = "Nixon";
+}
+
+
+let nixon = document.getElementById("nixon");
+
+nixon.addEventListener("mouseover", function(){
+    nixon.src ="fNixon.png"
+})
+nixon.addEventListener("mouseout", function(){
+    nixon.src = "nixonfc.png"
+})
+
