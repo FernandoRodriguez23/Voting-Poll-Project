bf6577c3281caba85a5370f6ed2f5031a7cb727b
Fernando Rodriguez Agredano <Frodri300@west-mec.local>
2021-02-05 18:13:38 -0700

    Finished

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 
+
+//This Will alert the user when onload
 function disCliamer() {
     alert("Please be sure to cast your vote for the next president");
 }
+document.getElementById("load").onload = disCliamer();
+
+//===========================================================================//
+
+//This changes the name on the button
 
 function changeGeorge(){
     document.getElementById("bGeorge").innerHTML = "Nixon";
 }
-
 function removeGeorge() {
     this.removeEventListener("click", changeGeorge);
 }
+
+//============================================================================//
+
+//The image of Nixon will change when you hover over it
 
 let nixon = document.getElementById("nixon");
 
@@ -19,6 +29,10 @@ nixon.addEventListener("mouseover", function(){
 nixon.addEventListener("mouseout", function(){
     nixon.src = "nixonfc.png"
 })
+
+//===================================================//
+
+//Soccer Field with a USA polandball and the Country's Election result as the  cursor
 
 field.onclick = function (event) {
     let fieldCord = this.getBoundingClientRect();
@@ -38,9 +52,29 @@ field.onclick = function (event) {
     ball.style.top = ballCord.top+'px'
 
 }
+//=============================================================//
+
+//If you inspect on the american flag it will alert you
 
 let contentMenu = document.getElementById("contentMenu");
 
 contentMenu.addEventListener('contextmenu', tempElem => {
     alert("Yup that's an American Flag");
 })
+
+//=============================================================//
+
+//If you click Nixon's Button, it will create links to a wiki page about the elections in the footer
+
+let bNixon = document.getElementById("bNixon");
+this.bNixon = createNixon();
+
+function createNixon() {
+    for(let i = 0; i <= 18; i++){
+        let newNixon = document.createElement("a");
+        newNixon.textContent = "Election Results";
+        newNixon.href = "https://en.wikipedia.org/wiki/1972_United_States_presidential_election#:~:text=The%201972%20United%20States%20presidential,George%20McGovern%20of%20South%20Dakota";
+        results.appendChild(newNixon);
+    }
+}
+
